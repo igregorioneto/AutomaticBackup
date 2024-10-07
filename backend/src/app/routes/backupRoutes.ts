f738303a2c5632dc9c24dbb5a10aa8a5c213f8b0
igregorioneto <igregoriodoneto@outlook.com>
2024-10-07 20:15:55 -0300

    feat: configure swagger

--- backend/src/app/routes/backupRoutes.ts
+++ backend/src/app/routes/backupRoutes.ts
@@ -24,6 +24,35 @@ const backupController = new BackupController(
   deleteBackupService
 );
 
+/**
+ * @swagger
+ * /backups:
+ *  get:
+ *    summary: Return Find All Backup
+ *    responses:
+ *      200:
+ *        description: Success
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: object
+ *              properties:
+*                  name:
+*                    type: string
+*                    example: Backup 1
+*                  type:
+*                    type: string
+*                    example: aws
+*                  status:
+*                    type: string
+*                    example: INITIALIZED
+*                  config:
+*                    type: object
+*                    example: {}
+*                  id:
+*                     type: number
+*                     example: 1
+ */
 router.get('/', async (req, res, next) => {
   try {
     await backupController.findAll(req, res);
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,7 @@ import express, { Request, Response } from "express";
 import cors from "cors";
 import scheduleRoutes from "./app/routes/scheduleRoutes";
 import backupRoutes from "./app/routes/backupRoutes";
+import { setupSwagger } from "./swagger";
 
 const app = express();
 const port = 3000;
@@ -14,6 +15,8 @@ app.use(express.urlencoded({ extended: true }));
 // Routes
 app.use('/schedules', scheduleRoutes);
 app.use('/backups', backupRoutes);
+
+setupSwagger(app);
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
