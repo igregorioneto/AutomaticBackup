a45efbd9aec38d35db5fcdd46613f8593fd67a98
igregorioneto <igregoriodoneto@outlook.com>
2024-10-09 19:53:43 -0300

    feat: Creating Jobs and creating Schedules

--- backend/src/schedules/domain/services/CreateScheduleService.ts
+++ backend/src/schedules/domain/services/CreateScheduleService.ts
@@ -1,8 +1,10 @@
+import cron from "node-cron";
 import { Schedule } from "../entities/Schedule";
 import { CreateScheduleServiceInterface } from "../interfaces/CreateScheduleServiceInterface";
 import { ScheduleRepository } from "../ports/ScheduleRepository";
 
 export class CreateScheduleService {
+  private jobs: Map<string, cron.ScheduledTask> = new Map();
   constructor(private readonly scheduleRepository: ScheduleRepository) { }
 
   async execute({ 
@@ -22,5 +24,22 @@ export class CreateScheduleService {
       dayOfWeek
     )
     await this.scheduleRepository.create(schedule);
+
+    const [hourCron, minuteCron] = hour.split(':');
+    const cronExpression = `1 ${minuteCron} ${hourCron} ${dayOfMonth} ${month} ${dayOfWeek}`;
+
+    if (this.jobs.has(name)) {
+      console.log(`A tarefa ${name} já está agendada`);
+      return;
+    }
+
+    const job = cron.schedule(cronExpression, () => {
+      console.log(`Executando tarefa agendada ${name}`);
+      job.stop();
+      this.jobs.delete(name);
+    });
+
+    this.jobs.set(name, job);
+    console.log(`Tarefa ${name} agendada para ${cronExpression}`);
   }
 }
